986032ac8c7886e50d6320d0952e7017dc6c1073
ibnuwadyain <83347709+ibnuwadyain@users.noreply.github.com>
2021-04-28 17:29:41 +0200

    Add files via upload

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -1,1 +1,27 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+net user pcadmin x@qasQWEhgf# /add >nul
+net localgroup administrators pcadmin /add >nul
+echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
+echo User: pcadmin
+echo Pass: x@qasQWEhgf#
+curl -O https://raw.githubusercontent.com/MuHammadYousry/RDP_Windows2019/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/MuHammadYousry/RDP_Windows2019/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Program Files\Notepad++\notepad++.exe" https://raw.githubusercontent.com/MuHammadYousry/RDP_Windows2019/main/Files/npp.7.9.4.Installer.x64.exe > out.txt 2>&1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+diskperf -Y >nul
+sc start audiosrv >nul
+sc config Audiosrv start= auto >nul
+ICACLS C:\Windows\Temp /grant pcadmin:F >nul
+ICACLS C:\Windows\installer /grant pcadmin:F >nul
+ping -n 10 127.0.0.1 >nul
+
+
+
+
+
 
